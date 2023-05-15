a00c884aad7448d856d9497d4ed6567ea0681dab
John Raymark Del Puerto <surjon16@gmail.com>
2023-05-15 11:20:04 +0800

    fix-window-calling

--- web/data/repositories/appointments.py
+++ web/data/repositories/appointments.py
@@ -19,7 +19,8 @@ class AppointmentsRepo:
         return Appointments.query.filter(and_(Appointments.status_id==3, Appointments.account_id==id, func.date(Appointments.schedule) == datetime.now().date())).order_by(Appointments.id.asc()).first()
 
     def readCalls():
-        return Appointments.query.filter(and_(Appointments.status_id.in_([2,3]), func.date(Appointments.schedule) == datetime.now().date())).group_by(Appointments.account_id).having(func.min(Appointments.id)).order_by(Appointments.updated_at.desc()).all()
+        # return Appointments.query.filter(and_(Appointments.status_id.in_([2,3]), func.date(Appointments.schedule) == datetime.now().date())).order_by(Appointments.updated_at.desc()).all()
+        return Appointments.query.filter(and_(Appointments.status_id.in_([3]), func.date(Appointments.schedule) == datetime.now().date())).group_by(Appointments.id).having(func.min(Appointments.id)).order_by(Appointments.updated_at.desc()).all()
 
     def readHistory(id):
         return Appointments.query.filter(and_(Appointments.status_id.in_([1,2]), Appointments.account_id==id, func.date(Appointments.schedule) == datetime.now().date())).order_by(Appointments.updated_at.desc()).limit(5).all()
