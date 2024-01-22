ebb9a10864a78094b4648ff15eb69c2e3f100ce2
surjon16 <surjon16@gmail.com>
2024-01-22 09:49:40 +0800

    enable-debugging

--- web/app.py
+++ web/app.py
@@ -7,5 +7,5 @@ app.WTF_CSRF_ENABLED = True
 if __name__ == '__main__':
     # db.drop_all()
     db.create_all()
-    # app.run(debug=True, host='0.0.0.0', port='8080')
-    app.run(host='0.0.0.0', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='8080')
+    # app.run(host='0.0.0.0', port='8080')
