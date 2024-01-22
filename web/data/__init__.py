35bb887295c1ccbd6ec1c8e5f2b2ea234352c025
surjon16 <surjon16@gmail.com>
2024-01-22 09:53:51 +0800

    retract-app-config-changes

--- web/data/__init__.py
+++ web/data/__init__.py
@@ -8,13 +8,14 @@ from flask_httpauth import HTTPBasicAuth
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/db_hpk' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-app.config['SQLALCHEMY_POOL_SIZE'] = 20
-app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 5
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 10
+# app.config['SQLALCHEMY_POOL_SIZE'] = 20
+# app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20
+# app.config['SQLALCHEMY_POOL_TIMEOUT'] = 5
+# app.config['SQLALCHEMY_POOL_RECYCLE'] = 10
 
 
-db = SQLAlchemy(app, session_options={'autocommit': True})
+# db = SQLAlchemy(app, session_options={'autocommit': True})
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # WEB VIEWS WITH LOGIN REQUIRED
